5f65b941fcd7167ad986370e8bcf503d59f89df4
Jamie-Walton <71863256+Jamie-Walton@users.noreply.github.com>
2023-03-11 18:46:55 -0800

    Use frontend build directory

--- hab-main/backend/settings.py
+++ hab-main/backend/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'frontend/build/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
