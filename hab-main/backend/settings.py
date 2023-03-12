2891af466435efc6b69b1a30e8eac643c050c392
Jamie-Walton <71863256+Jamie-Walton@users.noreply.github.com>
2023-03-11 18:52:40 -0800

    Remove frontend pointers to build

--- hab-main/backend/settings.py
+++ hab-main/backend/settings.py
@@ -119,14 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static')
+    os.path.join(BASE_DIR, 'build/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/build/static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'build/static/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
